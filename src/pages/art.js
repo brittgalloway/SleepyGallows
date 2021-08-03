d32b9028f20d86ef618aeba2f3265c141c4a8d7a
Brittney G <brittney.galloway@cleverbridge.com>
2021-08-03 13:49:22 -0500

    images on illustration page

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,8 +1,13 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 const ArtPage = () => {
     return (
+      <>
     <h1>Art</h1>
+    <Link to="/crystalsart">Crystal</Link>
+    <Link to="/brittneysart">brittney</Link>
+    </>
     )
   }
   
--- src/pages/crystalsart.js
+++ src/pages/crystalsart.js
@@ -1,9 +1,34 @@
-import * as React from "react"
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage } from "gatsby-plugin-image"
 
-const CrystalPage = () => {
-    return (
-    <h1>Crystal's Art</h1>
-    )
+
+//   export default CrystalPage
+const IllustrationPage = ({ data }) => (
+  <article>
+    {data.allDatoCmsIllustraion.edges.map(({node})=> (
+      <div>
+      <GatsbyImage image={node.art.gatsbyImageData} alt={node.art.alt}/>
+      <p>{node.art.title}</p> 
+      </div>
+    ))}
+  </article>
+)
+
+  export default IllustrationPage
+
+export const query = graphql`
+query ArtQuery {
+  allDatoCmsIllustraion {
+    edges {
+      node {
+        art {
+          gatsbyImageData(placeholder: BLURRED)
+          alt
+          title
+        }
+      }
+    }
   }
-  
-  export default CrystalPage
+}
+`
