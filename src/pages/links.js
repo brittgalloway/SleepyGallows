4d38ad69d942d75f4c39501138fc162999d493f0
Brittney G <brittney.galloway@cleverbridge.com>
2021-08-03 13:48:55 -0500

    styled Link page

--- src/pages/links.js
+++ src/pages/links.js
@@ -4,11 +4,46 @@ import styled from "styled-components"
 import '../styles/global.css'
 
 const StyledDiv = styled.div`
-      nav{
-        font-family: var(--textFont);
+    background: linear-gradient(180deg,#b5dbff,#d0c1f4);
+    width: 100%;
+    height:650px;
+    @media (min-width: 481px){
+        height: 1300px;
     }
+    @media (max-width: 480px){
+        height: 900px;
+    }
+    section{
+        text-align:center;
+    }
+    nav{
+        font-family: var(--linkFont);
+        font-size: 2rem;
+        ul{
+            list-style:none;
+            padding:0;
+            li{
+                padding: 1rem;
+                a{
+                    text-decoration: none;
+                    color: var(--brand-color);
+                    &:hover{
+                        color: var(--pink-highlight);
+                    }
+                }
+            }
+        }
+    }
+  
     h1{
         font-family: var(--brandFont);
+        font-size: 3rem;
+        color: var(--brand-color);
+        margin: 0;
+        padding: 2rem;
+        @media (max-width: 480px){
+            font-size: 2rem;
+        }
     }
 
 
@@ -17,29 +52,31 @@ const StyledDiv = styled.div`
 const Links = () => {
     return (
     <StyledDiv>
-        <h1>Sleepy Gallows Links</h1>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Offical Website</Link>
-                </li>
-                <li>
-                    <a className="linkList" href="https://ko-fi.com/sleepygallows">Ko-fi</a>
-                </li>
-                <li>
-                    <a className="linkList" href="https://brittgalloway.github.io/SG_Blog/">Blog</a>
-                </li>
-                <li>
-                    <a className="linkList" href="https://brittgalloway.github.io/memory_game/">PLH Memory Game</a>
-                </li>
-                <li>
-                    <a className="linkList" href="https://www.patreon.com/2heroes">2Heroes Patreon</a>
-                </li>
-                <li>
-                    <a className="linkList" href="https://www.candyfluffs.com/2heroes">2Heroes Necahual</a>
-                </li>
-            </ul>
-        </nav>
+        <section>
+            <h1>Sleepy Gallows Links</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Offical Website</Link>
+                    </li>
+                    <li>
+                        <a className="linkList" href="https://ko-fi.com/sleepygallows">Ko-fi</a>
+                    </li>
+                    <li>
+                        <a className="linkList" href="https://brittgalloway.github.io/SG_Blog/">Blog</a>
+                    </li>
+                    <li>
+                        <a className="linkList" href="https://brittgalloway.github.io/memory_game/">PLH Memory Game</a>
+                    </li>
+                    <li>
+                        <a className="linkList" href="https://www.patreon.com/2heroes">2Heroes Patreon</a>
+                    </li>
+                    <li>
+                        <a className="linkList" href="https://www.candyfluffs.com/2heroes">2Heroes Necahual</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
     </StyledDiv>
     )
   }
