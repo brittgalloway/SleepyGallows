5300c941a85917b3d5f8b0270903465bd08f5aaa
Brittney G <brittney.galloway@cleverbridge.com>
2021-08-05 17:07:49 -0500

    added nav

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,13 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import '../styles/global.css'
+
+const StyledDiv = styled.div`
+
+`
 
 const ArtPage = () => {
     return (
-      <>
-    <h1>Art</h1>
-    <Link to="/crystalsart">Crystal</Link>
-    <Link to="/brittneysart">brittney</Link>
-    </>
+      <StyledDiv>
+          <Layout />
+          <main>
+          <h1>Art</h1>
+          <Link to="/crystalsart">Crystal</Link>
+          <Link to="/brittneysart">brittney</Link>
+          </main>
+      </StyledDiv>
     )
   }
   
