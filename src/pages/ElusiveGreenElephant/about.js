35e4b80142f5adc5749e400bed29410350b47111
BrittGall <crlnfllr@gmail.com>
2022-06-04 21:23:01 -0500

    update images and sizing

--- src/pages/ElusiveGreenElephant/about.js
+++ src/pages/ElusiveGreenElephant/about.js
@@ -7,23 +7,36 @@ import {SGFooter} from '../../components/footer'
 import Layout from '../../components/layout'
 
 const StyledDiv = styled.div`
+main {
+  @media (max-width: 830px){
+    padding: 0;
+  }
+}
 section {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content:center;
-  padding: 2rem;
+  padding: 1rem;
+  @media (max-width: 830px){
+    padding: 0;
+  }
 }
 main img{
-  // margin: 1rem 2rem;
-  // max-width: 450px;
-  min-height:300px;
+  margin: 1rem 2rem;
+  width: 100%;
+  min-height: 200px;
   @media (max-width: 830px){
     margin: 1rem 0;
   }
 }
 main div{
-  width:300px;
+  width: 500px;
+  @media (max-width: 830px){
+    margin: 1rem 0;
+    width: 100%;
+    min-height: 150px;
+  }
 }
 h1, h2{
   font-family: var(--brandFont);
@@ -33,6 +46,9 @@ h1, h2{
 p{
   font-size: 1rem;
   padding:none;
+  @media (max-width: 830px){
+    padding: 20px;
+  }
 }
 `
 
