cc432dcb1b48a786b6715de99e9a495ad7a5d045
BrittGall <crlnfllr@gmail.com>
2024-01-25 22:08:50 -0600

    began shop layout

---
+++ app/components/shopFooter.js
@@ -0,0 +1,25 @@
+'use client'
+import { useMediaQuery } from 'react-responsive'
+import Link from 'next/link'
+
+export function ShopFooter() {
+    const isMobile = useMediaQuery({query: `(max-width: 830px)`});
+    const year = new Date().getFullYear();
+    return (
+     <footer>
+        {isMobile ? 
+            <span>Social media</span> :
+            null
+        }
+        <p>Sleepy Gallows Studio</p>
+        <ul>
+            <li><Link href={'/animation'}>Animation</Link></li>
+            <li><Link href={'/comics'}>Comics</Link></li>
+            <li><Link href={'/art'}>Art</Link></li>
+            <li><Link href={'/about'}>About</Link></li>
+        </ul>
+        <iframe src='https://embeds.beehiiv.com/3178b493-940a-49e7-a1e0-c7095d94b9db?slim=true' frameBorder={0} data-test-id='beehiiv-embed' ></iframe>
+        <p>Copyright © {year} Sleepy Gallows Studio - All Rights Reserved.</p>
+     </footer>
+    );
+  }
