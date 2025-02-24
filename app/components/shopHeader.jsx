5b9f9ee3add86c6418c873dad67e7ca55e62e18e
brittgalloway <crlnfllr@gmail.com>
2025-02-24 10:08:39 -0600

    the client url is a ternary in stripe.js. removed unused code

--- app/components/shopHeader.jsx
+++ app/components/shopHeader.jsx
@@ -6,19 +6,14 @@ import { useMediaQuery } from 'react-responsive'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { userAccountLink } from '@/lib/stripe'
 import Checkout from './checkout'
 import ShopNavigation from './shopNav'
 import styles from '@/style/shopHeader.module.scss'
 
 export default function ShopHeader() {
 
-  const [isOpen, setIsOpen] = useState(false);
   const isMobile = useMediaQuery({query: `(max-width: 830px)`});
-
-  function handleClick() {
-    isOpen === true ? setIsOpen(false) : setIsOpen(true);
-  }
-
 
   return(
     <header className={styles.header}>
@@ -40,16 +35,15 @@ export default function ShopHeader() {
             alt="sleepy gallows logo"
             />
           </Link>
-            }
+        }
       {!isMobile&& (
         <h1 className={styles.h1}><Link href='/shop/'>Sleepy Gallows&apos; Shop</Link></h1>
-        )}
-          <p>Take some of the Magic with you</p>
-        </div>
-        {!isMobile&& (<a href='https://billing.stripe.com/p/login/test_dR615302V1Hz1K8fYY'><FontAwesomeIcon icon={faUser} /></a>)}
-        {/* {!isMobile&& (<a href='https://billing.stripe.com/p/login/aEU16L6Aq6EP7yE000'><FontAwesomeIcon icon={faUser} /></a>)} */}
-          <Checkout/>
-        </div>
+      )}
+        <p>Take some of the Magic with you</p>
+      </div>
+      {!isMobile&& (<a href={userAccountLink}><FontAwesomeIcon icon={faUser} /></a>)}
+        <Checkout/>
+      </div>
       <ShopNavigation/>
     </header>
   )
