4acd010d06dc4d250421da4fe7ac8dbc3202e35c
brittgalloway <crlnfllr@gmail.com>
2025-02-25 12:41:43 -0600

    added suspense

--- app/shop/patron/thank_you_patron/page.jsx
+++ app/shop/patron/thank_you_patron/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function ThankYouPatron() {
     const searchParams = useSearchParams();
@@ -9,14 +10,16 @@ export default function ThankYouPatron() {
     return (
         <div>
             <h1>Thank You for Becoming a Patron!</h1>
-            {promoCode ? (
-              <div>
-                <p>Your exclusive promotion code: <strong>{promoCode}</strong></p>
-                <p>Expires in {promoLength}</p>
-              </div>
-            ) : (
-                <p>Thank you for your support!</p>
-            )}
+            <Suspense>
+              {promoCode ? (
+                <div>
+                  <p>Your exclusive promotion code: <strong>{promoCode}</strong></p>
+                  <p>Expires in {promoLength}</p>
+                </div>
+              ) : (
+                  <p>Thank you for your support!</p>
+              )}
+            </Suspense>
             <p>A coupn code should be sent to your email within 24 hours.</p>
             <p>If not, please reach out to bg@sleepygallows.com</p>
         </div>
