6dbfd4c685ef7ce53455ac75634c80aa80735041
brittgalloway <crlnfllr@gmail.com>
2025-02-25 17:39:18 -0600

    persist cart data between stripe checkout and the cart

--- app/shop/cartContext.js
+++ app/shop/cartContext.js
@@ -1,10 +1,25 @@
 'use client'
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export const StateProvider = ({ children }) => {
   const [cart, setCart] = useState({ count: 0, items: [] });
+
+  // Load cart from local storage when the component mounts
+  useEffect(() => {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart.match(/"count":0/)) {
+      localStorage.removeItem('cart');
+    } else {
+      setCart(JSON.parse(storedCart));
+    }
+  }, []);
+  
+  // Save cart to local storage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, setCart }}>
