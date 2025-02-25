ed5f1846974bd9f090a98cb917a3ab6f4b43b648
brittgalloway <crlnfllr@gmail.com>
2025-02-25 12:46:26 -0600

    altered suspense implimentation

--- app/shop/patron/thank_you_patron/page.jsx
+++ app/shop/patron/thank_you_patron/page.jsx
@@ -2,23 +2,30 @@
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-export default function ThankYouPatron() {
-    const searchParams = useSearchParams();
-    const promoCode = searchParams.get('promo');
-    const promoLength = searchParams.get('interval');
+function PromoCodeDisplay() {
+  const searchParams = useSearchParams();
+  const promoCode = searchParams.get('promo');
+  const promoLength = searchParams.get('interval');
+  return (
+    <>
+      { promoCode ? (
+        <div>
+          <p>Your exclusive promotion code: <strong>{promoCode}</strong></p>
+          <p>Expires in {promoLength}</p>
+        </div>
+      ) : (
+          <p>Thank you for your support!</p>
+      )}
+    </>
+  )
+}
 
+export default function ThankYouPatron() {
     return (
         <div>
             <h1>Thank You for Becoming a Patron!</h1>
             <Suspense>
-              {promoCode ? (
-                <div>
-                  <p>Your exclusive promotion code: <strong>{promoCode}</strong></p>
-                  <p>Expires in {promoLength}</p>
-                </div>
-              ) : (
-                  <p>Thank you for your support!</p>
-              )}
+              <PromoCodeDisplay/>
             </Suspense>
             <p>A coupn code should be sent to your email within 24 hours.</p>
             <p>If not, please reach out to bg@sleepygallows.com</p>
