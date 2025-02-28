7e73a8f6ac11806feb6f345316b3fbf405925d4c
brittgalloway <crlnfllr@gmail.com>
2025-02-28 14:52:31 -0600

    added strip webhook variable

--- app/lib/stripe.js
+++ app/lib/stripe.js
@@ -9,6 +9,8 @@ export const env = process.env.ENVIRONMENT || 'sandbox';
 const STRIPE_SECRET = env === 'sandbox' ? process.env.STRIPE_SECRET_TEST : process.env.STRIPE_SECRET;
 const STRIPE_PUBLIC = env === 'sandbox' ? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST : process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
+export const STRIPE_WH_SECRET = env === 'sandbox' ? process.env.STRIPE_WH_SECRET_TEST : process.env.STRIPE_WH_SECRET;
+
 export const stripe = new Stripe(STRIPE_SECRET);
 export const stripePromise = loadStripe(STRIPE_PUBLIC);
 
