a2f2a0db34704034e31abf82b78407d9f0055b1f
Brittney G <brittney.galloway@cleverbridge.com>
2025-03-11 12:05:59 -0500

    fixed AddToCart to work if there are no prevItems

--- app/components/addToCart.jsx
+++ app/components/addToCart.jsx
@@ -1,25 +1,30 @@
 'use client'
-import { useEffect } from "react"
-import { useCartContext } from "@/shop/cartContext"
+import { useEffect } from 'react'
+import { useCartContext } from '@/shop/cartContext'
 
-export default function AddToCart({ product, discount, stock, price, productDescription}) {
+export default function AddToCart({ product, discount, stock, price, productDescription }) {
     const { cart, setCart } = useCartContext();
+
     const handleCart = () => {
         setCart((prevCart) => {
-            const existingItemIndex = prevCart.items.findIndex(
+            const prevItems = prevCart?.items || []; // Ensure we handle an empty cart properly
+            const existingItemIndex = prevItems.findIndex(
                 (item) => item.price === product?.default_price
             );
-    
+
             let updatedItems;
+
             if (existingItemIndex !== -1) {
-                updatedItems = prevCart.items.map((item, index) =>
+                updatedItems = prevItems.map((item, index) =>
                     index === existingItemIndex
-                        ? item.productStock > 1 ? { ...item, quantity: item.quantity + 1 } : item
+                        ? item.productStock > item.quantity
+                            ? { ...item, quantity: item.quantity + 1 }
+                            : item
                         : item
                 );
             } else {
                 updatedItems = [
-                    ...prevCart.items,
+                    ...prevItems,
                     {
                         price: product?.default_price,
                         quantity: 1,
@@ -28,25 +33,25 @@ export default function AddToCart({ product, discount, stock, price, productDesc
                         productPrice: price,
                         productDiscount: discount,
                         productDescription: productDescription,
-                        productDisplay: product?.images[0],
+                        productDisplay: product?.images?.[0],
                     },
                 ];
             }
+
             const newCount = updatedItems.reduce((total, item) => total + item.quantity, 0);
+
             return {
                 count: newCount,
                 items: updatedItems,
             };
         });
     };
-    
 
     useEffect(() => {}, [cart]);
 
     return (
         <button type="button" disabled={stock <= 0} onClick={handleCart} onKeyDown={handleCart}>
             Add To Cart
-        </button> 
+        </button>
     );
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
