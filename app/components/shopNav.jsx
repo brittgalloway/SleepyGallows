334a108e45208997c2a491a372f122ff9174b35a
brittgalloway <crlnfllr@gmail.com>
2025-05-17 00:53:25 -0500

    shop nav is popover

--- app/components/shopNav.jsx
+++ app/components/shopNav.jsx
@@ -7,39 +7,43 @@ import { USER_ACCOUNT_LINK } from '@/lib/stripe'
 import styles from '@/style/shopHeader.module.scss'
 
 export default function ShopNavigation() {
-  const [isOpen, setIsOpen] = useState(false);
 
   const isMobile = useMediaQuery({query: `(max-width: 830px)`});
-  function handleClick() {
-    isOpen === true ? setIsOpen(false) : setIsOpen(true);
-  }
   const links = ['fine-art', 'prints', 'stickers', 'books', 'patron'];
+
   return(
-    <nav aria-label='Main shop navigation' className={isMobile && isOpen ? `${styles.mobile} ${styles.nav}` : styles.nav} onClick={handleClick}>
-      {isMobile && 
+    <nav aria-label='Main shop navigation' className={isMobile ? `${styles.mobile} ${styles.nav}` : styles.nav}>
+    { isMobile ? (
+      <>
         <div className={styles.mobile_menu} >
-          <button className={styles.hamburger} onClick={handleClick}>
+          <button className={styles.hamburger} data-testid="toggle-shop-nav-button" type="button" popovertarget="shop-nav">
             <Image src='/hamburger.svg' 
             width={40} 
             height={30} 
             alt="hamburger menu"/>
           </button>
+        <dialog data-testid="shop-nav" id="shop-nav" popover="true">
+           <ul aria-label="Shop Menu" className={styles.nav_list}>
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link href={`/shop/${link}`}>{link.replace('-', ' ')}</Link></li>
+            ))}
+            <li>
+                <a href={USER_ACCOUNT_LINK}>Account</a>
+            </li>
+          </ul>
+        </dialog>
         </div>
-      }
+      </>
+    ) : (
+      <ul aria-label="Shop Menu" className={styles.nav_list}>
+        {links.map((link, index) => (
+          <li key={index}>
+            <Link href={`/shop/${link}`}>{link.replace('-', ' ')}</Link></li>
+        ))}
+      </ul>
+    )}
       
-      {(isOpen || !isMobile) && (
-        <ul aria-label="Shop Menu" className={styles.nav_list}>
-          {links.map((link, index) => (
-            <li key={index}>
-              <Link href={`/shop/${link}`}>{link.replace('-', ' ')}</Link></li>
-          ))}
-          {isMobile && 
-              <li>
-                  <a href={USER_ACCOUNT_LINK}>Account</a>
-              </li>
-          }
-        </ul>
-      )}
     </nav>
   )
 }
