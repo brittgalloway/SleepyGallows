43148c18e3d0370abbd2d86fe577afb8cd841eef
Brittney G <brittney.galloway@cleverbridge.com>
2025-05-28 16:09:19 -0500

    fixed html error

--- app/components/CartProduct.jsx
+++ app/components/CartProduct.jsx
@@ -71,7 +71,7 @@ export function CartProduct() {
                 min={1}
                 max={item.productStock}
                 size={3}
-                maxlength={3}
+                maxLength={3}
                 value={item.quantity}
                 onChange={(e) => handleQty(e, item.price)}
               />
