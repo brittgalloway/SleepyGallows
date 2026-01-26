deb66582ca69075688bb3338d3d570037c30ec96
brittgalloway <crlnfllr@gmail.com>
2026-01-26 00:05:43 -0600

    shipping placeholder

--- app/api/create_checkout/route.js
+++ app/api/create_checkout/route.js
@@ -41,8 +41,45 @@ export async function POST(req) {
       },
       shipping_options: [
         {
-          shipping_rate:'shr_1QuqfTJiAoJrRPIxEZjH27YW',
-        }
+      shipping_rate_data: {
+        type: 'fixed_amount',
+        fixed_amount: {
+          amount: 0,
+          currency: 'usd',
+        },
+        display_name: 'Free shipping',
+        delivery_estimate: {
+          minimum: {
+            unit: 'business_day',
+            value: 5,
+          },
+          maximum: {
+            unit: 'business_day',
+            value: 7,
+          },
+        },
+      },
+    },
+        {
+      shipping_rate_data: {
+        type: 'fixed_amount',
+        fixed_amount: {
+          amount: 1500,
+          currency: 'usd',
+        },
+        display_name: 'Next day air',
+        delivery_estimate: {
+          minimum: {
+            unit: 'business_day',
+            value: 1,
+          },
+          maximum: {
+            unit: 'business_day',
+            value: 1,
+          },
+        },
+      },
+    },
       ],
       invoice_creation: {
         enabled: true,
