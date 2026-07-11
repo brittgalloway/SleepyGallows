6dfba5d7f34c88d98c26eddff283e24c6c6a481f
brittgalloway <crlnfllr@gmail.com>
2026-07-10 23:29:22 -0500

    small style adjustments, flipped stripe conditional

--- app/lib/stripe.server.js
+++ app/lib/stripe.server.js
@@ -3,9 +3,9 @@ import Stripe from 'stripe'
 
 const env = process.env.ENVIRONMENT || 'sandbox';
 
-const STRIPE_SECRET = env === 'sandbox'
-  ? process.env.STRIPE_SECRET_TEST
-  : process.env.STRIPE_SECRET;
+const STRIPE_SECRET = env !== 'sandbox'
+  ? process.env.STRIPE_SECRET
+  : process.env.STRIPE_SECRET_TEST;
 
 if (!STRIPE_SECRET) {
   throw new Error(`Missing Stripe secret key for environment: ${env}`);
@@ -13,17 +13,17 @@ if (!STRIPE_SECRET) {
 
 export const stripe = new Stripe(STRIPE_SECRET);
 
-export const STRIPE_WH_SECRET = env === 'sandbox'
-  ? process.env.STRIPE_WH_SECRET_TEST
-  : process.env.STRIPE_WH_SECRET;
+export const STRIPE_WH_SECRET = env !== 'sandbox'
+  ? process.env.STRIPE_WH_SECRET
+  : process.env.STRIPE_WH_SECRET_TEST;
 
-export const PATRON_PRODUCT = env === 'sandbox'
-  ? (process.env.STRIPE_PATRON_PRODUCT_TEST ?? 'prod_RnZ1n8UFz12j3c')
-  : process.env.STRIPE_PATRON_PRODUCT;
+export const PATRON_PRODUCT = env !== 'sandbox'
+  ? (process.env.STRIPE_PATRON_PRODUCT ?? 'prod_RnZ1n8UFz12j3c')
+  : process.env.STRIPE_PATRON_PRODUCT_TEST;
 
-export const PATRON_COUPON = env === 'sandbox'
-  ? (process.env.STRIPE_PATRON_COUPON_TEST ?? '6BusZfcF')
-  : process.env.STRIPE_PATRON_COUPON;
+export const PATRON_COUPON = env !== 'sandbox'
+  ? process.env.STRIPE_PATRON_COUPON
+  : process.env.STRIPE_PATRON_COUPON_TEST;
 
 if (!PATRON_PRODUCT || !PATRON_COUPON) {
   console.warn(`[stripe.server] PATRON_PRODUCT or PATRON_COUPON not set for environment: ${env}`);
